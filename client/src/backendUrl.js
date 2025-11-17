b51d1dc01468dbf72f37569b9f22b8fbebcef9e5
Harshal <Harshalself7@gmail.com>
2025-11-17 21:18:21 +0530

    Update Package json

--- client/src/backendUrl.js
+++ client/src/backendUrl.js
@@ -1,4 +1,4 @@
 const backendUrl = "https://unstuck-five.vercel.app";
-// const backendUrl = "https://8s6msbr1-3005.inc1.devtunnels.ms";
+// const backendUrl = "localhost";
 
 export default backendUrl;
